2807fd654916afdd603184757e0e2f1ddc504121
ethan <17emp5@queensu.ca>
2020-12-26 22:12:04 -0500

    cmsis based blink

---
+++ PlatformIO/pmboard/cmsis/src/main.cpp
@@ -0,0 +1,25 @@
+#include "stm32f446xx.h"
+
+#define LED_PORT GPIOB
+#define LED_DDR GPIOB
+#define LED 1
+
+volatile int tester = 0;
+
+void delay() {
+    for (volatile long long i = 0; i < 500000; i++);
+}
+
+int main(void) {
+    SystemCoreClockUpdate();
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN; // enable GPIO port B  clock
+    LED_PORT->MODER |= LED; // set bit 0 to output
+    LED_PORT->ODR = 0;
+    
+    for (;;) {
+        LED_PORT->ODR ^= LED;
+        delay();
+        tester++;
+    }
+    
+}
