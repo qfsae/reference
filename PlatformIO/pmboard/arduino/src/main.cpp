d034144734d11de0ca1eebf589c9c78f402fe901
ethan <17emp5@queensu.ca>
2020-12-26 21:29:07 -0500

    blink fixup

--- PlatformIO/pmboard/arduino/src/main.cpp
+++ PlatformIO/pmboard/arduino/src/main.cpp
@@ -2,13 +2,13 @@
 
 // Acquired from looking at the arduino to STM pin mapping
 // https://github.com/stm32duino/Arduino_Core_STM32/blob/master/variants/Generic_F446Rx/variant.h
-#define LED 16
+#define LED A8
 
 int ledState = 0;
 
 void setup() {
 	Serial2.begin(9600);
-//	pinMode(LED, OUTPUT);
+	pinMode(LED, OUTPUT);
 }
 
 void loop() {
