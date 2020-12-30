4fec9564e77ba7c2114f77a355c98978e8a823bd
ethan <17emp5@queensu.ca>
2020-12-29 23:26:26 -0500

    add note about PIO arduino environment

--- PlatformIO/pmboard/arduino/src/main.cpp
+++ PlatformIO/pmboard/arduino/src/main.cpp
@@ -2,7 +2,10 @@
 
 // Acquired from looking at the arduino to STM pin mapping
 // https://github.com/stm32duino/Arduino_Core_STM32/blob/master/variants/Generic_F446Rx/variant.h
+// NOTE: Arduino layer in platformio supports CMSIS mapping of MCU registers
+// Thus, all the low level code we write will be directly portable to the Arduino environment in platformio
 #define LED A8
+
 
 int ledState = 0;
 
